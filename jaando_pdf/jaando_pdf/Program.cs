9f69b97f1c9c767a9f8bb8be86cd6823569611da
D.S <dominik@jaando-software.de>
2015-08-26 21:06:47 +0200

    Adds initial files for mono-ocr

---
+++ jaando_pdf/jaando_pdf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace jaando_pdf
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string monitoringPath = args [0];
+			string destinationPath = args [1];
+			string user = args [2];
+			string group = args [3];
+
+			DirectoryInfo monitoringPathInfo = new DirectoryInfo (monitoringPath);
+			DirectoryInfo destinationPathInfo = new DirectoryInfo (destinationPath);
+
+			foreach(
+		}
+	}
+}
